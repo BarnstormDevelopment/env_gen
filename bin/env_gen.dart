e102e72289ef462505a600890c716160bbc0f1c2
Nathaniel Heitsch <nathanielheitsch@iMac.local>
2022-04-24 13:15:18 -0500

    init

---
+++ bin/env_gen.dart
@@ -0,0 +1,5 @@
+import 'package:env_gen/env_gen.dart' as env_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${env_gen.calculate()}!');
+}
---
+++ test/env_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:env_gen/env_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
